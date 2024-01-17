e2dbcd901b78633d12b86269d11d552de6191c62
resana febin <resanafazil417@gmail.com>
2024-01-17 11:49:04 +0530

    Initial commit

---
+++ MyButton.js
@@ -0,0 +1,17 @@
+// MyButton.js
+
+import React from 'react';
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+export default MyButton;
